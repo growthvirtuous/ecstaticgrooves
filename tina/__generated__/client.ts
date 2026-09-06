fd4e4d3644166620341c762a874a3b0215358420
growthvirtuous <growth@virtuousalliance.com>
2026-09-06 18:56:06 +0200

    Fascia pre-footer con invito al movimento

--- tina/__generated__/client.ts
+++
@@ -1,5 +0,0 @@
-import { createClient } from "tinacms/dist/client";
-import { queries } from "./types.js";
-export const client = createClient({ url: 'http://localhost:4001/graphql', token: 'a3c6636393c79d19a6848a50c7e7f516a61c87d3', queries,  });
-export default client;
-  
